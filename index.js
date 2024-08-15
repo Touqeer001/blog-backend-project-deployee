f98b2dbb8b3fe40e149e7797479ad80b82ccbc05
Touqeer Ansari <touqeeransari001@gmail.com>
2024-08-15 22:12:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import path from 'path';
+
+
+//components
+import Connection from "./database/db.js";
+import Router from "./routes/route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//routes
+
+app.use("/", Router);
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname); 
+app.use('/file', express.static(path.join(__dirname, 'uploads')));
+
+const PORT = 8080;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+Connection(username, password);
+
+app.listen(PORT, () =>
+  console.log(`Server is running successfully on PORT ${PORT}`)
+);
